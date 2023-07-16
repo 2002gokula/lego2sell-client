bdd3b41b459e33b313f8a636cd51f1dd84219a56
Gokula krishnan R <105733059+2002gokula@users.noreply.github.com>
2023-07-16 11:46:39 +0530

    update

---
+++ src/componet/PDFComplete.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react"
+import { degrees, PDFDocument, rgb, StandardFonts } from "pdf-lib"
+import download from "downloadjs"
+
+const PDFModificationExample = () => {
+  const handleModifyPdf = async () => {
+    const url = "./SellyournewsetsatLEGO2sell.pdf"
+    // console.log(url)
+    const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer())
+
+    const pdfDoc = await PDFDocument.load(existingPdfBytes)
+    const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
+
+    const pages = pdfDoc.getPages()
+    const firstPage = pages[0]
+    const { width, height } = firstPage.getSize()
+    firstPage.drawText("This text was added with JavaScript!", {
+      x: 5,
+      y: height / 2 + 300,
+      size: 50,
+      font: helveticaFont,
+      color: rgb(0.95, 0.1, 0.1),
+      rotate: degrees(-45),
+    })
+
+    const pdfBytes = await pdfDoc.save()
+
+    // Trigger the browser to download the PDF document
+    download(pdfBytes, "SellyournewsetsatLEGO2sell.pdf", "application/pdf")
+  }
+
+  return (
+    <div>
+      <p>
+        Click the button to modify an existing PDF document with{" "}
+        <code>pdf-lib</code>
+      </p>
+      <button onClick={handleModifyPdf}>Modify PDF</button>
+      <p className="small">(Your browser will download the resulting file)</p>
+    </div>
+  )
+}
+
+export default PDFModificationExample
